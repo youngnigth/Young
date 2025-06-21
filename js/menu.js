348f50286f7e7d0fdd0786bb455847644fb96023
youngnigth <youngdesert01@gmail.com>
2025-06-20 20:50:25 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,8 @@
+const toggleMenu = document.getElementById('menu-toggle');
+const navMenu = document.querySelector('.nav');
 
+if (toggleMenu && navMenu) {
+  toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+}
