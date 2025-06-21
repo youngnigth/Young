c3a91f9abc2cef15c2d928287c9a1652ab8e32dd
youngnigth <youngdesert01@gmail.com>
2025-06-20 20:50:13 -0400

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
-const toggleMenu = document.getElementById('menu-toggle');
-const navMenu = document.querySelector('.nav');
-
-if (toggleMenu && navMenu) {
-  toggleMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-  });
-}
